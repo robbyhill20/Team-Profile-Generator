17bb06d2b9e6f993bd1fe9f7841eff63a70c3773
Robby Hill <robbyhill20@yahoo.com>
2022-03-07 07:55:25 -0800

    html and some testing

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require("../lib/Employee");
+
+
+test('Testing Employee', () => {
+  const emp = new Employee('BobTester', 1, 'bobtest@testing.com');
+
+  expect(emp.name).toBe('BobTester');
+  expect(emp.id).toBe(1);
+  expect(emp.email).toBe('bobtest@testing.com');
+  
+  expect(emp.getName()).toBe('BobTester');
+  expect(emp.getId()).toBe(1);
+  expect(emp.getEmail()).toBe('bobtest@testing.com');
+  expect(emp.getRole()).toBe('Employee');
+});
